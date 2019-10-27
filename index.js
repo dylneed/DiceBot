bd8411702bb6eb26fcbb34c50fed507b3db519a2
Dylan Needleman <Dylneed@Dylneed.local>
2019-10-27 17:52:58 -0400

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ return (1 + Math.floor(n * Math.random()));}
 function Convert(spliter, string) {
 	return parseInt(string.split(spliter)[1])
 }
-
+function NumToLettBad(n) {
+if (n === 1) {return 'A'} else
+if (n === 2) {return 'B'} else
+if (n === 3) {return 'C'} else
+if (n === 4) {return 'D'}}
 
 
 const Discord = require('discord.js'); 
@@ -13,13 +17,34 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`); 
 });
 client.on('message', msg => {  
-    if (msg.content.includes('Hello Die', 0)) {     
-        msg.reply('Hello');}
-    if (msg.content.includes('Roll ', 0)) {
-    	msg.reply(Roll(Convert('Roll ',msg.content)));}
-    if (msg.content.includes('roll', 0)) {
-    	msg.reply(Roll(Convert('roll ',msg.content)));}
+    if (msg.content.includes('Hello Die', 0))      
+        {msg.reply('Hello');}
+        
+        
+    if (msg.content.includes('Roll ', 0)) 
+    	{msg.reply(Roll(Convert('Roll ',msg.content)));}
+    if (msg.content.includes('roll', 0))
+    	{msg.reply(Roll(Convert('roll ',msg.content)));}
+    	
+    	
     if (msg.content.includes( "up up down down left right left right b a" , 0)) 
-       {msg.reply('Fuck you ')}
-})
+        {msg.reply('Fuck you ')}
+   
+   mention = message.mentions.users.first();
+   if (msg.content.includes( "&&DM", 0)){
+   	   if (mention == null) {return;}
+      message.delete();
+      mentionMessage = message.content.slice(8);
+      mention.sendMessage(mentionMessage);
+      message.channel.send("Message sent to" + message.mentions.users.first();)
+   }})
+
+
+
+
+
+
+
+
+
 client.login('NjM3ODM3NTExNjgyNDI0ODMz.XbUB8g.fqnRq_qBQYXQi9uRNygr1nx5Jr0');
