b5292b70797dbe63e1087952abcfeac9af4f3eb1
Dylan Needleman <Dylneed@Dylneed.local>
2019-10-27 20:40:30 -0400

    make it better

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('message', msg => {
    	   var user1 = msg.mentions.users.first();
    	   var msg2 = msg.split(" ")[2];
    	   var user2 = msg2.mentions.users.first();
-   	   msg.reply("hello " + user2);
+   	   msg.reply("hello " + msg1 + ' ' + msg2);
    }
    	   
    })
