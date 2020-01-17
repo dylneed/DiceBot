179fea4339a155b6519321f16da560efe2d44cfa
Dylan <dylneed@gmail.com>
2020-01-17 16:58:41 -0500

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function Roll(n) {
 return (1 + Math.floor(n * Math.random()));}
 
+var OrderedDeck = new Array ("AC","2C","3C","4C","5C","6C","7C","8C","9C",
+	"10C","JC","QC","KC","AH","2H","3H","4H","5H","6H","7H","8H","9H",
+	"10H","JH","QH","KH","AS","2S","3S","4S","5S","6S","7S","8S","9S",
+	"10S","JS","QS","KS","AD","2D","3D","4D","5D","6D","7D","8D","9D",
+	"10D","JD","QD","KD");
+
 function Convert(spliter, string) {
 	return parseInt(string.split(spliter)[1])
 }
@@ -153,14 +159,36 @@ function Face(num) {
 }
 
 
-function Card(num) {
+function Card(card /*in the format AH etc.*/) {
+	var num = 0;
+	for (num = 0;num<OrderedDeck.length;num++) {
+		if (OrderedDeck[num] === card) {break;} else
+		if (num === OrderedDeck.length - 1) {num = "ERROR";}
+	}
 	var n = num % 52;
-	var s = Math.floor(n/13);
-	var f = n % 13;
+	var s = (Math.floor(n/13) + 1)%4;
+	var f = ((n + 1) % 13);
 	var out = Face(f) + Suit(s);
 	return out;
 }
-      
+
+function Shuffle(a) {
+    var s = new Array();
+    for (i=0;i<a.length;i++) {
+    	var R = Roll(a.length) - 1;
+        while (s.includes(R)) {
+        	R = Roll(a.length) - 1;
+        }
+        s[i] = R;
+    }
+    var out = new Array();
+    for (i=0;i<a.length;i++) {
+    	out[i] = a[s[i]]
+    }
+    return out;
+}
+
+var ShuffledDeck = Shuffle(OrderedDeck);
 
 
 
